51d72ad23194f9227fa3bc98b4c78841679c5918
Richard Opiyo <romenyo@dewcis.com>
2022-03-15 20:39:06 +0300

    fix accroding to code review report

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
-import './App.css';
 import Calculator from './components/Calculator';
-// import Calculator from './components/Calculator';
-
-// export default function App() {
-//   return (
-//     <Calculator />
-//   );
-// }
 
 class App extends React.Component {
   render() {
@@ -17,17 +9,5 @@ class App extends React.Component {
     );
   }
 }
-// function App() {
-//   return (
-//     <div className="App">
-//       {/* <header className="App-header"> */}
-//       <h1>Say hello</h1>
-//       {/* <Calculator /> */}
-//       {/* <Calculator /> */}
-//       {/* </header> */}
-//       {/* <Calculator /> */}
-//     </div>
-//   );
-// }
 
 export default App;
