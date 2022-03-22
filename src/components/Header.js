16696f20724bab2d4049babf8f7201d9ee9782dc
Richard Opiyo <romenyo@dewcis.com>
2022-03-22 22:12:33 +0300

    addition

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './HeaderStyles.css';
+// import { BrowserRouter as Link } from 'react-router-dom';
 
 const Header = () => (
   <header className="headsection">
@@ -8,6 +9,7 @@ const Header = () => (
     </h1>
     <ul className="lists">
       <a href="/" className="list-item">Home</a>
+      {/* <Link to={{ pathname: '/' }} className="list-item">Home</Link> */}
       <a href="/Calculator" className="list-item">Calculator</a>
       <a href="/Quotes" className="list-item">Quotes</a>
     </ul>
